da9eb9442ea49442128834a131738a112dd75029
noahschmid <noah.schmid77@gmail.com>
2019-10-30 15:38:22 +0100

    Fix error with product model

--- backend/api/models/product.js
+++ backend/api/models/product.js
@@ -1,12 +1,12 @@
-const mongoose = import('mongoose');
+const mongoose = require('mongoose');
 
-const productSchema = {
-    _id:mongoose.Schema.types.ObjectId,
+const productSchema = mongoose.Schema({
+    _id:mongoose.Schema.Types.ObjectId,
     name: { type:String },
     category: { type:String },
     price: { type:Number },
     verified: { type:Boolean, default:false },
     seller: { type:Object, default:{} }
-}
+});
 
 module.exports = mongoose.model('Product', productSchema);
