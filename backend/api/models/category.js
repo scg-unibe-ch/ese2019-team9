c5c28dd5ae4d9b72e14a9ab01a97ba99fbfea03e
noahschmid <noah.schmid77@gmail.com>
2019-11-04 15:49:50 +0100

    Change category system to a materialized path based system

--- backend/api/models/category.js
+++ backend/api/models/category.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const categorySchema = mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
     name:{ type:String, required:true },
-    parent:mongoose.Schema.Types.ObjectId,
-    slug:{ type:String },
-    subcategories:{ type:Array, default:[] },
-    products:{ type:Array, default:[] }
+    parent:{ type:String, index:true },
+    path:{ type:String, index:true },
+    slug:{ type:String }
 });
 
-module.exports = mongoose.model('Category', categorySchema);
+module.exports = mongoose.model('Category2', categorySchema);
