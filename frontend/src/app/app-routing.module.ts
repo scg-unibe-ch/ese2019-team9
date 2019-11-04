b80ff8d0be3142d18fc778cbecacaca098553fd2
Lorenz Caliezi <lorenz.caliezi@students.unibe.ch>
2019-11-04 15:29:14 +0100

    Migrate homePage into folder pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RoleguardService } from './core/services/roleguardService/roleguard.ser
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
   { path: 'verify', loadChildren: './pages/verify/verify.module#VerifyPageModule' },
   { path: 'registered', loadChildren: './pages/registered/registered.module#RegisteredPageModule' },
   { path: 'subcategory', loadChildren: './pages/subcategory/subcategory.module#SubcategoryPageModule' },
