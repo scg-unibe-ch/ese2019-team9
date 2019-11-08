c36d11caa34ca383f1c4a8959a864ff17fa4d732
Lorenz Caliezi <lorenz.caliezi@students.unibe.ch>
2019-11-08 17:50:45 +0100

    Add subcategoryMenu for mobile burgerMenu and fix subcategoryMenu in navbar

--- frontend/src/app/core/header/navigation-bar/navigation-bar.component.ts
+++ frontend/src/app/core/header/navigation-bar/navigation-bar.component.ts
@@ -10,6 +10,7 @@ export class NavigationBarComponent implements OnInit {
   categories = [];
   menuVisible = false;
   currentMenuSubcategories;
+  currentEvent;
 
   constructor(
       private categoryService: CategoryService
@@ -18,31 +19,26 @@ export class NavigationBarComponent implements OnInit {
   ngOnInit() {
     this.categoryService.getCategories().subscribe(data => {
       // @ts-ignore
-      this.categories = data.categories;
+      this.categories = data;
     });
   }
 
   segmentChanged(ev: any) {
-    this.categoryService.getSingleCategoryFromSlug(ev.detail.value)
-        .subscribe(data => {
-          // @ts-ignore
-          this.currentMenuSubcategories = data.categories[0].subcategories.sort((a, b) => a.name.localeCompare(b.name));
-        });
+    if (ev.target.value === '') {
+      return;
+    }
+    this.currentEvent = ev;
+    this.currentMenuSubcategories = this.categories.filter(cat => cat.slug === ev.detail.value)[0].subcategories
+        .sort((a, b) => a.name.localeCompare(b.name));
     this.menuVisible = true;
   }
 
   dismissMenu() {
+    if (this.menuVisible === false) {
+      return;
+    }
+    this.currentEvent.target.value = '';
     this.menuVisible = false;
+    this.currentMenuSubcategories = [];
   }
-
-  /*
-  const popover = await this.popoverController.create({
-      component: CategoryMenuComponent,
-      componentProps: { subcategories: this.currentMenuSubcategories},
-      translucent: true,
-      backdropDismiss: true,
-      cssClass: 'subcategoryMenuPopover'
-    });
-    return await popover.present();
-   */
 }
