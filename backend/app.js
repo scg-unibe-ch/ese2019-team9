9aa9c65fe21dbc66ef3b383048c817de1ff3babe
noahschmid <noah.schmid77@gmail.com>
2019-11-11 17:21:53 +0100

    Add get route for images

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
 
