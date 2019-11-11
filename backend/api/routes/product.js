9c7e8eb3bd69657dff010439c832c3c5c7c6a31d
noahschmid <noah.schmid77@gmail.com>
2019-11-11 15:09:25 +0100

    Add new middleware to get user information

--- backend/api/routes/product.js
+++ backend/api/routes/product.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const ProductController = require('../controllers/product');
 const checkAuth = require('../mware/check-auth');
+const getUserInfo = require('../mware/get-user-info');
 const imageUpload = require('../mware/image-upload');
 
-router.get('/', ProductController.getProducts);
+router.get('/', getUserInfo, ProductController.getProducts);
 
 router.get('/:productId', ProductController.getProductById);
 
