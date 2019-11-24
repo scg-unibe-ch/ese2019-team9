046843a91baac0288b8a3efbce575d50a5aa2964
Adrian Jörg <adi.joerg@hotmail.com>
2019-11-24 23:27:21 +0100

    implemented first draft of filtering

--- frontend/src/app/core/services/filterAndSearchService/filter-and-search.service.ts
+++ frontend/src/app/core/services/filterAndSearchService/filter-and-search.service.ts
@@ -17,21 +17,21 @@ export class FilterAndSearchService {
 	 * @param caseSensitive optional boolean parameter if the search should be caseSensitive
 	 * @param skipFieldNames optional string Array of fieldnames to skip
 	 */
-	search(array: Object[], searchTerm: string, fieldnames?: string[], caseSensitive: boolean = false, skipFieldNames?: string[]) :Set<({obj, key, indices})> {
+	search(array: Object[], searchTerm: string, fieldnames?: string[], caseSensitive: boolean = false, skipFieldNames?: string[]): Set<({ obj, key, indices })> {
 		if (searchTerm.length < 1) return new Set();
 		searchTerm = caseSensitive ? searchTerm : searchTerm.toUpperCase();
 		if (!fieldnames) {
 			fieldnames = this.getAllKeys(array);
 		}
 		if (skipFieldNames) {
-			fieldnames = fieldnames.filter(element => 
+			fieldnames = fieldnames.filter(element =>
 				skipFieldNames.indexOf(element) === -1
 			);
 		}
 
-		if(fieldnames.length < 1) return new Set();
+		if (fieldnames.length < 1) return new Set();
 
-		let result = new Set<{obj, key, indices}>();
+		let result = new Set<{ obj, key, indices }>();
 		fieldnames.forEach((key) => {
 			array.forEach((obj) => {
 				if (obj.hasOwnProperty(key)) {
@@ -66,20 +66,20 @@ export class FilterAndSearchService {
 		 * @return a Set of Objects containing a key with the object that was found and a key with a map for all fieldnames and their corresponding indices
 		 * return example: ``` Set<{obj: Object, app: Map<fieldname: string, indices: number[]>}> ``` 
 		 */
-	searchUnique(array: Object[], searchTerm: string, fieldnames?: string[], caseSensitive: boolean = false, skipFieldNames?: string[]) :Set<{obj, app}> {
+	searchUnique(array: Object[], searchTerm: string, fieldnames?: string[], caseSensitive: boolean = false, skipFieldNames?: string[]): Set<{ obj, app }> {
 		let resultWithMultipleEntries = this.search(array, searchTerm, fieldnames, caseSensitive, skipFieldNames);
-		let result = new Set<{obj, app: Map<string, number[]>}>();
+		let result = new Set<{ obj, app: Map<string, number[]> }>();
 		let objToMap = new Map<object, Map<string, number[]>>();
 		resultWithMultipleEntries.forEach((entry) => {
-			if (!objToMap.has(entry.obj)){
+			if (!objToMap.has(entry.obj)) {
 				let map = new Map<string, number[]>();
 				map.set(entry.key, entry.indices);
-				result.add({obj: entry.obj, app: map});
+				result.add({ obj: entry.obj, app: map });
 				objToMap.set(entry.obj, map);
-			}else {
+			} else {
 				objToMap.get(entry.obj).set(entry.key, entry.indices);
 			}
-		});	
+		});
 		return result;
 	}
 
@@ -90,15 +90,15 @@ export class FilterAndSearchService {
 	 * @return the sorted array
 	 */
 	sort(array: Object[], ...args: string[]): Object[] {
-		return array.sort(function (a,b) {
+		return array.sort(function (a, b) {
 			let result = 0;
-			for (let i = 0; i < args.length && result == 0; i++){
-				let currentArgument:string  = args[i];
-				let sortOrder: boolean = currentArgument.slice(0,1) == "+" ? true : false;
+			for (let i = 0; i < args.length && result == 0; i++) {
+				let currentArgument: string = args[i];
+				let sortOrder: boolean = currentArgument.slice(0, 1) == "+" ? true : false;
 				let argument: string = args[i].substring(1);
-				if (typeof (a[argument]) == 'string' || typeof b[argument] == 'string'){
+				if (typeof (a[argument]) == 'string' || typeof b[argument] == 'string') {
 					result = sortOrder ? ('' + a[argument]).toLocaleLowerCase().localeCompare('' + b[argument].toLocaleLowerCase()) : ('' + b[argument]).toLocaleLowerCase().localeCompare('' + a[argument].toLocaleLowerCase());
-				}else{
+				} else {
 					result = sortOrder ? a[argument] - b[argument] : b[argument] - a[argument];
 				}
 			}
@@ -112,19 +112,19 @@ export class FilterAndSearchService {
 	 * @param args argument in the type of ```'op;fieldname;value'``` where ```op``` is an operator of therse types:
 	 * ```>, <, =, !=, >=, <=, ^, $, inc, isNull, isNotNull``` ^ means starts with, $ means ends with, inc means includes
 	 */
-	filter(array: {}[], args: string) :{}[] {
+	filter(array: {}[], args: string): {}[] {
 		let opMap = new Map<string, Function>([
-			['>',  (a,b) =>  a > b],
-			['<',  (a,b) =>  a < b],
-			['=',  (a,b) =>  a == b],
-			['!=',  (a,b) =>  a != b],
-			['>=',  (a,b) =>  a >= b],
-			['<=',  (a,b) =>  a <= b],
-			['^',  (a,b) =>  (a as string).startsWith(b as string)],
-			['$',  (a,b) =>  (a as string).endsWith(b as string)],
-			['inc',  (a,b) =>  (a as string).includes(b as string)],
-			['isNull',  (a,b) =>  isNullOrUndefined(a)],
-			['isNotNull',  (a,b) => !isNullOrUndefined(a)],
+			['>', (a, b) => a > b],
+			['<', (a, b) => a < b],
+			['=', (a, b) => a == b],
+			['!=', (a, b) => a != b],
+			['>=', (a, b) => a >= b],
+			['<=', (a, b) => a <= b],
+			['^', (a, b) => (a as string).startsWith(b as string)],
+			['$', (a, b) => (a as string).endsWith(b as string)],
+			['inc', (a, b) => (a as string).includes(b as string)],
+			['isNull', (a, b) => isNullOrUndefined(a)],
+			['isNotNull', (a, b) => !isNullOrUndefined(a)],
 		]);
 		const split = args.split(';');
 		if (split.length < 3) return [];
@@ -143,7 +143,7 @@ export class FilterAndSearchService {
 	 * @param args argument in the type of ```'op;fieldname;value'``` where ```op``` is an operator of therse types:
 	 * ```>, <, =, !=, >=, <=, ^, $, inc``` ^ means starts with, $ means ends with, inc means includes
 	 */
-	public filterComplex(array: {}[], args: string[]){
+	public filterComplex(array: {}[], args: string[]) {
 		let allObjects: {}[][] = [];
 		args.forEach((arg) => {
 			allObjects.push(this.filter(array, arg));
@@ -166,8 +166,8 @@ export class FilterAndSearchService {
 		return Array.from(keys) as string[];
 	}
 
-	public filterToObject(arg: string): {name: string, operator: string, value: string} {
+	public filterToObject(arg: string): { name: string, operator: string, value: string } {
 		const split = arg.split(';');
-		return {name: split[1], operator: split[0], value: split[2]};
+		return { name: split[1], operator: split[0], value: split[2] };
 	}
 }
--- frontend/src/app/pages/subcategory/subcategory.page.ts
+++ frontend/src/app/pages/subcategory/subcategory.page.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {PopoverController} from '@ionic/angular';
-import {ProductService} from '../../core/services/productService/product.service';
-import {CategoryService} from '../../core/services/categoryService/category.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../core/services/productService/product.service';
+import { CategoryService } from '../../core/services/categoryService/category.service';
 import { FilterAndSearchService } from 'src/app/core/services/filterAndSearchService/filter-and-search.service';
 import { ProgressIndicatorService } from 'src/app/core/services/progressIndicatorService/progress-indicator.service';
+import { isUndefined } from 'util';
+import { IonRange } from '@ionic/angular';
 
 
 @Component({
@@ -18,19 +19,20 @@ export class SubcategoryPage implements OnInit {
     private products: {}[] = [];
     private featuredProducts = [];
     private filledStars = 0;
-    private priceSpan: {lower: number, upper: number} = {lower: 0, upper: 1000};
+    private priceSpan: { lower: number, upper: number } = { lower: 0, upper: 1000 };
 
-    private filterSettings: {rating: string, location: string, minPrice: string, maxPrice: string} = {rating: '', location: '', minPrice: '', maxPrice: ''};
+    private filterSettings: { rating: string, location: string, minPrice: string, maxPrice: string } =
+        { rating: '', location: '', minPrice: '', maxPrice: '' };
     private filterargs: string[] = [];
+    private lastfilterargs: string[] = [];
     private subcategory;
     private carouselIsReady = false;
 
     constructor(private route: ActivatedRoute,
-                private productService: ProductService,
-                private categoryService: CategoryService,
-                private popoverController: PopoverController,
-                private filterAndSearchService: FilterAndSearchService,
-                private progressIndicatorService: ProgressIndicatorService) {
+        private productService: ProductService,
+        private categoryService: CategoryService,
+        private filterAndSearchService: FilterAndSearchService,
+        private progressIndicatorService: ProgressIndicatorService) {
     }
 
     /*
@@ -51,9 +53,9 @@ export class SubcategoryPage implements OnInit {
         this.carouselIsReady = true;
     }
 
-    updateProducts(){
+    updateProducts() {
         return this.productService.getProductsById(this.subcategory._id).then(products => {
-            //@ts-ignore
+            // @ts-ignore
             this.allProducts = products;
             this.products = this.applyFilters(this.allProducts);
             this.getPriceSpan();
@@ -61,69 +63,164 @@ export class SubcategoryPage implements OnInit {
         });
     }
 
-    applyFilters(products: {}[]) : {}[] {
+    applyFilters(products: {}[]): {}[] {
         this.filterargs = [];
         Object.keys(this.filterSettings).forEach((key) => {
-            if (this.filterSettings[key] != ''){
+            if (this.filterSettings[key] !== '') {
                 this.filterargs.push(this.filterSettings[key]);
             }
         });
-        if (this.filterargs.length > 0)
-        return this.filterAndSearchService.filterComplex(products, this.filterargs);
+        if (this.filterargs.length > 0 && !this.filterDidNotChange(this.filterargs, this.lastfilterargs)) {
+            return this.filterAndSearchService.filterComplex(products, this.filterargs);
+        }
         return products;
     }
 
-    filterargsToDisplay(): {}[] {
-        let filterObjArray: {name: string, operator: string, value: string}[] = [];
-        this.filterargs.forEach((el)=> {
-            filterObjArray.push(this.filterAndSearchService.filterToObject(el));
+    filterDidNotChange(arr1: string[], arr2: string[]): boolean {
+        if (arr1 === arr2) { return true; }
+        if (arr1 === null || arr2 === null) { return false; }
+        if (arr1.length !== arr2.length) { return false; }
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) { return false; }
+        }
+        return true;
+    }
+
+    get filterargsToDisplay(): { name: string, operator: string, value: string }[] {
+        const displayNames: Map<string, Map<string, string>> = new Map<string, Map<string, string>>(
+            [
+                ['price', new Map<string, string>(
+                    [
+                        ['>=', 'minPrice'],
+                        ['<=', 'maxPrice']
+                    ]
+                )]
+            ]
+        );
+        const filterObjArray: { name: string, operator: string, value: string, displayName: string }[] = [];
+        this.filterargs.forEach((el) => {
+            const filterObj = this.filterAndSearchService.filterToObject(el);
+            const name = filterObj.name;
+            const operator = filterObj.operator;
+            const map = (displayNames.has(name)) ? displayNames.get(name) : undefined;
+            const displayName = (map && map.get(operator)) ? map.get(operator) : filterObj.name;
+            filterObjArray.push({ name: filterObj.name, operator: filterObj.operator, value: filterObj.value, displayName });
         });
-        return this.filterargsToDisplay();
+        return filterObjArray;
+    }
+
+    removeFilter(filter: { name: string, operator: string, value: string, displayName?: string }) {
+        const resetFilterInput: Map<string, Function> = new Map<string, Function>(
+            [
+                ['rating', () => { this.filledStars = 0; }],
+                ['maxPrice', () => {
+                    (document.querySelector('#maxPriceInput') as HTMLInputElement).value = this.priceSpan.upper.toString();
+                    const element = ((document.querySelector('#priceRangeInput') as unknown) as IonRange);
+                    this.setRangeValueTo((element.value as any).lower, this.priceSpan.upper);
+                }],
+                ['minPrice', () => {
+                    (document.querySelector('#minPriceInput') as HTMLInputElement).value = this.priceSpan.lower.toString();
+                    const element = ((document.querySelector('#priceRangeInput') as unknown) as IonRange);
+                    this.setRangeValueTo(this.priceSpan.lower, (element.value as any).upper);
+                }]
+            ]
+        );
+        if (!filter.displayName) {
+            filter.displayName = filter.name;
+        }
+        if (this.filterSettings[filter.displayName] !== '') {
+            this.filterSettings[filter.displayName] = '';
+            this.products = this.applyFilters(this.allProducts);
+            if (resetFilterInput.has(filter.displayName)) {
+                resetFilterInput.get(filter.displayName)();
+            }
+        }
+    }
+
+    setRangeValueTo(lower: number, upper: number) {
+        const element = ((document.querySelector('#priceRangeInput') as unknown) as IonRange);
+        element.value = { lower, upper };
     }
 
     displayLoading(promise: Promise<void>) {
-        this.progressIndicatorService.presentLoading("Updating products");
+        this.progressIndicatorService.presentLoading('Updating products');
         promise.then(() => {
             this.progressIndicatorService.dismissLoadingIndicator();
-            this.progressIndicatorService.presentToast("Successfully updated products", 2000);
+            this.progressIndicatorService.presentToast('Successfully updated products', 2000);
         }, () => {
             this.progressIndicatorService.dismissLoadingIndicator();
-            this.progressIndicatorService.presentToast("Products couldn't be updated", 2000, "danger");
+            this.progressIndicatorService.presentToast('Products couldn\'t be updated', 2000, 'danger');
         });
     }
 
     onRatingFilterChanged(n: number) {
-        this.filterSettings.rating = `>;rating;${n}`;
-        let promise = this.updateProducts();
+        this.filterSettings.rating = `>=;rating;${n}`;
+        const promise = this.updateProducts();
         this.displayLoading(promise);
     }
 
     onPriceSliderChange(evt) {
-        let newPrice: {lower: number, upper: number} = evt.detail.value;
-        this.onMinPriceChange(newPrice.lower, false);
-        this.onMaxPriceChange(newPrice.upper, false);
+        const newPrice: { lower: number, upper: number } = evt.target.value;
+        this.onMinPriceChange(newPrice.lower);
+        this.onMaxPriceChange(newPrice.upper);
     }
 
-    onMinPriceChange(n: number, updateProducts: boolean) {
-
+    onMinPriceChange(n?: number) {
+        console.log(n);
+        const input: HTMLInputElement = document.querySelector('#minPriceInput');
+        if (!isUndefined(n)) {
+            if (input.value !== n.toString()) {
+                input.value = n.toString();
+            }
+            const element = ((document.querySelector('#priceRangeInput') as unknown) as IonRange);
+            this.setRangeValueTo(n, (element.value as any).upper);
+        } else {
+            const newValue: number = Number(input.value);
+            if (isNaN(newValue)) { return; }
+            n = newValue;
+        }
+        if (n === 0) {
+            this.removeFilter({ name: 'minPrice', operator: '>=', value: '' });
+        } else {
+            this.filterSettings.minPrice = `>=;price;${n}`;
+        }
+        this.products = this.applyFilters(this.allProducts);
     }
 
-    onMaxPriceChange(n: number, updateProducts: boolean) {
-
+    onMaxPriceChange(n: number) {
+        const input: HTMLInputElement = document.querySelector('#maxPriceInput');
+        if (!isUndefined(n)) {
+            if (input.value !== n.toString()) {
+                input.value = n.toString();
+            }
+            const element = ((document.querySelector('#priceRangeInput') as unknown) as IonRange);
+            this.setRangeValueTo((element.value as any).lower, n);
+        } else {
+            const newValue: number = Number(input.value);
+            if (isNaN(newValue)) { return; }
+            n = newValue;
+        }
+        if (n === this.priceSpan.upper) {
+            this.removeFilter({ name: 'maxPrice', operator: '<=', value: '' });
+        } else {
+            this.filterSettings.maxPrice = `<=;price;${n}`;
+        }
+        this.products = this.applyFilters(this.allProducts);
     }
 
-    array(n: number) :number[] {
-        let arr = Array(n);
-        return [...arr.keys()].map(ind => ind+1);
-      }
+    array(n: number): number[] {
+        const arr = Array(n);
+        return [...arr.keys()].map(ind => ind + 1);
+    }
 
-  fillTo(n: number) :void {
-    this.filledStars = n;
-  }
+    fillTo(n: number): void {
+        this.filledStars = n;
+    }
 
-  getPriceSpan(){
-        let sorted = this.filterAndSearchService.sort([...this.products], "+price");
-        this.priceSpan = {lower: sorted[0]['price'], upper: sorted[sorted.length-1]['price']}
-  }
+    getPriceSpan(): { lower: number, upper: number } {
+        const sorted = this.filterAndSearchService.sort([...this.products], '+price');
+        if (sorted.length === 0) { return { lower: 0, upper: 1000 }; }
+        this.priceSpan = { lower: sorted[0]['price'], upper: sorted[sorted.length - 1]['price'] };
+    }
 
 }
