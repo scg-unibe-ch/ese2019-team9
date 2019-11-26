34b769c9dc3dac0359161dafe7c11e3ccfdac86c
Lorenz Caliezi <lorenz.caliezi@students.unibe.ch>
2019-11-26 17:13:55 +0100

    Add button in profile to add product

--- frontend/src/app/pages/profile/user-products/user-products.component.ts
+++ frontend/src/app/pages/profile/user-products/user-products.component.ts
@@ -1,50 +1,55 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {AuthService} from '../../../core/services/authService/auth.service';
 import {ProductService} from '../../../core/services/productService/product.service';
 import {ProgressIndicatorService} from '../../../core/services/progressIndicatorService/progress-indicator.service';
 
 @Component({
-  selector: 'app-user-products',
-  templateUrl: './user-products.component.html',
-  styleUrls: ['./user-products.component.scss'],
+    selector: 'app-user-products',
+    templateUrl: './user-products.component.html',
+    styleUrls: ['./user-products.component.scss'],
 })
-export class UserProductsComponent implements OnInit {
-  userId;
-  products = [];
-  valuesToHide = ['_id', 'id', 'category', 'seller', 'reviews', 'verified', 'image', 'toRevise', 'rating'];
-  additionalValues = ['category', 'verified', 'rating', 'toRevise'];
+export class UserProductsComponent implements OnInit, OnDestroy {
+    userId;
+    products = [];
+    valuesToHide = ['_id', 'id', 'category', 'seller', 'reviews', 'verified', 'image', 'toRevise', 'rating', 'openDetail', 'date'];
+    additionalValues = ['category', 'verified', 'rating', 'toRevise', 'date'];
 
-  constructor(
-      private authService: AuthService,
-      private productService: ProductService,
-      private progressIndicatorService: ProgressIndicatorService
-  ) { }
+    constructor(
+        private authService: AuthService,
+        private productService: ProductService,
+        private progressIndicatorService: ProgressIndicatorService
+    ) {
+    }
 
-  ngOnInit() {
-    this.getUserProducts();
-  }
+    ngOnInit() {
+        this.getUserProducts();
+    }
 
-  getUserProducts() {
-    this.userId = this.authService.getId();
-    this.productService.getProductsByUserId(this.userId).subscribe(data => {
-      this.products = data;
-    }, err => {
-      console.log(err);
-      this.progressIndicatorService.presentToast('Products could not be updated', 2000, 'danger');
-    });
-  }
+    ngOnDestroy(): void {
+        this.getUserProducts();
+    }
 
-  deleteProduct(productId: string) {
-    this.productService.deleteProduct(productId).subscribe(data => {
-      this.progressIndicatorService.presentToast('Product successfully deleted', 2000, 'success');
-      this.reloadProducts();
-    }, err => {
-      console.log(err);
-      this.progressIndicatorService.presentToast('Product could not be deleted', 2000, 'danger');
-    });
-  }
+    getUserProducts() {
+        this.userId = this.authService.getId();
+        this.productService.getProductsByUserId(this.userId).subscribe(data => {
+            this.products = data;
+        }, err => {
+            console.log(err);
+            this.progressIndicatorService.presentToast('Products could not be updated', 2000, 'danger');
+        });
+    }
 
-  reloadProducts() {
-    this.getUserProducts();
-  }
+    deleteProduct(productId: string) {
+        this.productService.deleteProduct(productId).subscribe(data => {
+            this.progressIndicatorService.presentToast('Product successfully deleted', 2000, 'success');
+            this.reloadProducts();
+        }, err => {
+            console.log(err);
+            this.progressIndicatorService.presentToast('Product could not be deleted', 2000, 'danger');
+        });
+    }
+
+    reloadProducts() {
+        this.getUserProducts();
+    }
 }
