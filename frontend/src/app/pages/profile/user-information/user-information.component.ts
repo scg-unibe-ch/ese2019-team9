59d5a43d262c3db7a88ebe91baf7755bfddacbaa
Lorenz Caliezi <lorenz.caliezi@students.unibe.ch>
2019-11-29 16:30:52 +0100

    Implement new notifications and fix logic

--- frontend/src/app/pages/profile/user-information/user-information.component.ts
+++ frontend/src/app/pages/profile/user-information/user-information.component.ts
@@ -1,16 +1,16 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../../core/services/userService/user.service';
 import {AuthService} from '../../../core/services/authService/auth.service';
 import {ProgressIndicatorService} from '../../../core/services/progressIndicatorService/progress-indicator.service';
 import {NotificationService} from '../../../core/services/notificationService/notification.service';
-import {FilterAndSearchService} from '../../../core/services/filterAndSearchService/filter-and-search.service';
+
 
 @Component({
   selector: 'app-user-information',
   templateUrl: './user-information.component.html',
   styleUrls: ['./user-information.component.scss'],
 })
-export class UserInformationComponent implements OnInit, OnDestroy {
+export class UserInformationComponent implements OnInit {
   userId;
   user;
   keys = ['_id', 'email', 'address', 'name', 'website', 'country', 'phone', 'sex'];
@@ -21,26 +21,11 @@ export class UserInformationComponent implements OnInit, OnDestroy {
   constructor(
       private userService: UserService,
       private authService: AuthService,
-      private progressIndicatorService: ProgressIndicatorService,
-      private notificationService: NotificationService,
-      private sortService: FilterAndSearchService
+      private progressIndicatorService: ProgressIndicatorService
   ) { }
 
   ngOnInit() {
     this.getUserInformation();
-    this.getNotifications();
-    console.log('entered');
-  }
-
-  ngOnDestroy(): void {
-    console.log('left');
-    this.notificationService.setAllNotificationsToRead().subscribe(err => {
-      console.log(err);
-    });
-  }
-
-  ionViewDidLeave() {
-    console.log('will leave');
   }
 
   getUserInformation() {
@@ -63,29 +48,5 @@ export class UserInformationComponent implements OnInit, OnDestroy {
         this.user[key] = '';
       }
     });
-  }
-
-  getNotifications() {
-    this.notificationService.getSingleUsersNotifications().subscribe(
-        data => {
-          // @ts-ignore
-          this.notifications = this.sortService.sort(data, '+read', '+date');
-          console.log(this.notifications);
-        }, err => {
-          console.log(err);
-        }
-    );
-  }
-
-  deleteNotification(notificationId: string) {
-    this.notificationService.deleteNotification(notificationId).subscribe(
-        data => {
-          this.progressIndicatorService.presentToast('Notification deleted', 3500);
-          this.getNotifications();
-        }, err => {
-          this.progressIndicatorService.presentToast('Notification not deleted', 3500, 'danger');
-          console.log(err);
-        }
-    );
   }
 }
