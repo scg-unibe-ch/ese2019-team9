1588e74816414e2b2f2bf53fe34ac63a21f065ea
Lorenz Caliezi <lorenz.caliezi@students.unibe.ch>
2019-12-04 15:50:34 +0100

    Fix bug where you could not edit profile and your product

--- frontend/src/app/core/services/userService/user.service.ts
+++ frontend/src/app/core/services/userService/user.service.ts
@@ -7,23 +7,26 @@ import { AuthService } from '../authService/auth.service';
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private httpClient: HttpClient, private authService: AuthService) { }
+  constructor(
+      private httpClient: HttpClient,
+      private authService: AuthService) { }
 
   userEndpoint = 'https://moln-api.herokuapp.com/user';
 
   getAllUsers(): Observable<[]>{
     const token = this.authService.getToken();
     const headers = new HttpHeaders().set('Authorization', 'Bearer: ' + token);
-    return this.httpClient.get<[]>(this.userEndpoint, {headers: headers});
+    return this.httpClient.get<[]>(this.userEndpoint, {headers});
   }
 
   deleteUser(id) {
     const token = this.authService.getToken();
     const headers = new HttpHeaders().set('Authorization', 'Bearer: ' + token);
-    return this.httpClient.delete(this.userEndpoint+`/${id}`, {headers: headers});
+    return this.httpClient.delete(this.userEndpoint + `/${id}`, {headers});
   }
 
   updateUser(id: string, body: string, img: any) {
+    body = JSON.parse(body);
     const formData = new FormData();
     const token = this.authService.getToken();
     const headers = new HttpHeaders().set('Authorization', 'Bearer: ' + token);
@@ -32,29 +35,30 @@ export class UserService {
     Object.keys(body).forEach(key => {
       formData.append(key, body[key]);
     });
-    if(img)
+    if (img)
       formData.append('image', img);
-    return this.httpClient.patch(this.userEndpoint +`/${id}`, formData, {headers});
+    console.log(formData);
+    return this.httpClient.patch(this.userEndpoint + `/${id}`, formData, {headers});
   }
 
   getSingleUser(id) {
     const token = this.authService.getToken();
     const headers = new HttpHeaders().set('Authorization', 'Bearer: ' + token);
-    return this.httpClient.get(this.userEndpoint+`/${id}`, {headers: headers});
+    return this.httpClient.get(this.userEndpoint + `/${id}`, {headers});
   }
   
   isSeller(): Promise<boolean>{
     return new Promise<boolean>((resolve, reject) => {
       if (!this.authService.isLoggedIn()) {return false;}
       const id = this.authService.getId();
-      this.getSingleUser(id).subscribe((data)=> {
+      this.getSingleUser(id).subscribe((data) => {
         if ((data as any).address && (data as any).address.length > 0 && (data as any).country 
         && (data as any).country.length > 0 && (data as any).name && (data as any).name.length > 0){
           resolve(true);
         }
         resolve(false);
       },
-      error=> {
+      error => {
         console.log(error);
         reject(error);
       });
